f62f0e1522e9b1dc4a7a5be89187c42e68b175fe
besuper <16976439+besuper@users.noreply.github.com>
2023-01-11 16:07:05 +0100

    Use github to fetch amazon worker

--- src/background.js
+++ src/background.js
@@ -12,31 +12,58 @@ if (isChrome) {
     // declarativeNetRequest only available on chrome
     chrome.declarativeNetRequest.updateDynamicRules({
         addRules: [{
+            'id': 1002,
+            'priority': 1,
+            'action': {
+                'type': "modifyHeaders",
+                'responseHeaders': [
+                    {
+                        'operation': "set",
+                        'header': 'content-type',
+                        'value': 'application/javascript',
+                    }
+                ]
+            },
+            'condition': {
+                'requestDomains': ['raw.githubusercontent.com']
+            }
+        },
+        {
             'id': 1001,
             'priority': 1,
             'action': {
                 'type': 'redirect',
                 'redirect': {
-                    url: chrome.runtime.getURL("src/amazon-ivs-worker.min.js")
+                    url: "https://raw.githubusercontent.com/besuper/TwitchNoSub/master/src/amazon-ivs-worker.min.js"
                 }
             },
             'condition': {
                 'urlFilter': 'https://static.twitchcdn.net/assets/amazon-ivs-wasmworker.min-*.js',
             }
         }],
-        removeRuleIds: [1001]
+        removeRuleIds: [1001, 1002]
     })
 } else {
     // Support firefox here
-    browser.webRequest.onBeforeRequest.addListener(function (details) {
-        return { redirectUrl: browser.runtime.getURL("src/amazon-ivs-worker.min.js") };
+    browser.webRequest.onBeforeRequest.addListener((details) => {
+        return { redirectUrl: "https://raw.githubusercontent.com/besuper/TwitchNoSub/master/src/amazon-ivs-worker.min.js" };
     },
         {
-            urls: [
-                "https://static.twitchcdn.net/assets/amazon-ivs-wasmworker.min-*.js"
-            ],
+            urls: ["https://static.twitchcdn.net/assets/amazon-ivs-wasmworker.min-*.js"],
             types: ["main_frame", "script"]
         },
         ["blocking"]
+    );
+
+    browser.webRequest.onHeadersReceived.addListener((details) => {
+        details.responseHeaders.push({
+            name: 'content-type',
+            value: 'application/javascript'
+        });
+
+        return { responseHeaders: details.responseHeaders };
+    },
+        { urls: ["https://raw.githubusercontent.com/besuper/TwitchNoSub/master/src/amazon-ivs-worker.min.js"] },
+        ["blocking", "responseHeaders"]
     );
 }
