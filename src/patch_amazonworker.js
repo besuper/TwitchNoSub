f8ee9746a4c649fb89bef70cdafc7d680e70a826
besuper <16976439+besuper@users.noreply.github.com>
2023-03-03 01:13:11 +0100

    Fix dead kraken api

--- src/patch_amazonworker.js
+++ src/patch_amazonworker.js
@@ -1,9 +1,54 @@
-async function fetchTwitchData(vodID) {
-    const resp = await fetch("https://api.twitch.tv/kraken/videos/" + vodID, {
-        method: 'GET',
+async function fetchTwitchDataGQL(vodID) {
+    const resp = await fetch("https://gql.twitch.tv/gql", {
+        method: 'POST',
+        body: JSON.stringify([
+            {
+                "operationName": "VideoPlayer_VODSeekbarPreviewVideo",
+                "variables": {
+                    "includePrivate": false,
+                    "videoID": vodID
+                },
+                "extensions": {
+                    "persistedQuery": {
+                        "version": 1,
+                        "sha256Hash": "07e99e4d56c5a7c67117a154777b0baf85a5ffefa393b213f4bc712ccaf85dd6"
+                    }
+                }
+            },
+            {
+                "operationName": "ComscoreStreamingQuery",
+                "variables": {
+                    "channel": "",
+                    "clipSlug": "",
+                    "isClip": false,
+                    "isLive": false,
+                    "isVodOrCollection": true,
+                    "vodID": vodID
+                },
+                "extensions": {
+                    "persistedQuery": {
+                        "version": 1,
+                        "sha256Hash": "e1edae8122517d013405f237ffcc124515dc6ded82480a88daef69c83b53ac01"
+                    }
+                }
+            },
+            {
+                "operationName": "VodChannelLoginQuery",
+                "variables": {
+                    "videoID": vodID
+                },
+                "extensions": {
+                    "persistedQuery": {
+                        "version": 1,
+                        "sha256Hash": "0c5feea4dad2565508828f16e53fe62614edf015159df4b3bca33423496ce78e"
+                    }
+                }
+            }
+        ]),
         headers: {
             'Client-Id': 'kimne78kx3ncx6brgo4mv6wki5h1ko',
-            'Accept': 'application/vnd.twitchtv.v5+json'
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
         }
     });
 
@@ -36,13 +81,38 @@ self.fetch = async function (url, opt) {
     if (url.startsWith("https://usher.ttvnw.net/vod/")) {
         if (response.status != 200) {
             const vodId = url.split("https://usher.ttvnw.net/vod/")[1].split(".m3u8")[0];
-            const data = await fetchTwitchData(vodId);
+            const data = await fetchTwitchDataGQL(vodId);
 
             if (data == undefined) {
                 return new Response("Unable to fetch twitch data API", 403);
             }
 
-            let resolutions = data.resolutions;
+            const videoData = data[0].data.video;
+            const vodData = data[1].data.video;
+            const channelData = data[2].data.video.owner
+
+            let resolutions = {
+                "160p30": {
+                    "res": "284x160",
+                    "fps": 30
+                },
+                "360p30": {
+                    "res": "640x360",
+                    "fps": 30
+                },
+                "480p30": {
+                    "res": "854x480",
+                    "fps": 30
+                },
+                "720p60": {
+                    "res": "1280x720",
+                    "fps": 60
+                },
+                "chunked": {
+                    "res": "1920x1080",
+                    "fps": 60
+                }
+            };
 
             let sorted_dict = Object.keys(resolutions);
             sorted_dict = sorted_dict.reverse();
@@ -55,7 +125,7 @@ self.fetch = async function (url, opt) {
 
             resolutions = ordered_resolutions;
 
-            const currentURL = new URL(data.animated_preview_url);
+            const currentURL = new URL(videoData.seekPreviewsURL);
 
             const domain = currentURL.host;
             const paths = currentURL.pathname.split("/");
@@ -66,30 +136,32 @@ self.fetch = async function (url, opt) {
             let sources_ = [];
 
             const now = new Date("2023-02-10");
-            const created = new Date(data.created_at);
+            const created = new Date(vodData.createdAt);
 
             const time_difference = now.getTime() - created.getTime();
             const days_difference = time_difference / (1000 * 3600 * 24);
 
-            if (data.broadcast_type === "highlight") {
+            const broadcastType = vodData.broadcastType.toLowerCase();
+
+            if (broadcastType === "highlight") {
                 for ([resKey, resValue] of Object.entries(resolutions)) {
                     sources_.push({
                         src: `https://${domain}/${vodSpecialID}/${resKey}/highlight-${vodId}.m3u8`,
-                        quality: resKey == "chunked" ? resValue.split("x")[1] + "p" : resKey,
-                        resolution: resValue,
-                        fps: Math.ceil(data.fps[resKey]),
+                        quality: resKey == "chunked" ? resValue.res.split("x")[1] + "p" : resKey,
+                        resolution: resValue.res,
+                        fps: Math.ceil(resValue.fps),
                         enabled: resKey == "chunked" ? "YES" : "NO"
                     });
                 };
-            } else if (data.broadcast_type === "upload" && days_difference > 7) {
+            } else if (broadcastType === "upload" && days_difference > 7) {
                 // Only old uploaded VOD works with this method now
 
                 for ([resKey, resValue] of Object.entries(resolutions)) {
                     sources_.push({
-                        src: `https://${domain}/${data.channel.name}/${vodId}/${vodSpecialID}/${resKey}/index-dvr.m3u8`,
-                        quality: resKey == "chunked" ? resValue.split("x")[1] + "p" : resKey,
-                        resolution: resValue,
-                        fps: Math.ceil(data.fps[resKey]),
+                        src: `https://${domain}/${channelData.login}/${vodId}/${vodSpecialID}/${resKey}/index-dvr.m3u8`,
+                        quality: resKey == "chunked" ? resValue.res.split("x")[1] + "p" : resKey,
+                        resolution: resValue.res,
+                        fps: Math.ceil(resValue.fps),
                         enabled: resKey == "chunked" ? "YES" : "NO"
                     });
                 };
@@ -97,9 +169,9 @@ self.fetch = async function (url, opt) {
                 for ([resKey, resValue] of Object.entries(resolutions)) {
                     sources_.push({
                         src: `https://${domain}/${vodSpecialID}/${resKey}/index-dvr.m3u8`,
-                        quality: resKey == "chunked" ? resValue.split("x")[1] + "p" : resKey,
-                        resolution: resValue,
-                        fps: Math.ceil(data.fps[resKey]),
+                        quality: resKey == "chunked" ? resValue.res.split("x")[1] + "p" : resKey,
+                        resolution: resValue.res,
+                        ps: Math.ceil(resValue.fps),
                         enabled: resKey == "chunked" ? "YES" : "NO"
                     });
                 }
