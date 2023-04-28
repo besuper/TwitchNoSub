a60ab6b88a680570b31d24055e7c553139da96b3
besuper <16976439+besuper@users.noreply.github.com>
2023-04-28 18:13:01 +0200

    Get only available vod qualities + better GQL request

--- src/patch_amazonworker.js
+++ src/patch_amazonworker.js
@@ -1,50 +1,9 @@
 async function fetchTwitchDataGQL(vodID) {
     const resp = await fetch("https://gql.twitch.tv/gql", {
         method: 'POST',
-        body: JSON.stringify([
-            {
-                "operationName": "VideoPlayer_VODSeekbarPreviewVideo",
-                "variables": {
-                    "includePrivate": false,
-                    "videoID": vodID
-                },
-                "extensions": {
-                    "persistedQuery": {
-                        "version": 1,
-                        "sha256Hash": "07e99e4d56c5a7c67117a154777b0baf85a5ffefa393b213f4bc712ccaf85dd6"
-                    }
-                }
-            },
-            {
-                "operationName": "ComscoreStreamingQuery",
-                "variables": {
-                    "channel": "",
-                    "clipSlug": "",
-                    "isClip": false,
-                    "isLive": false,
-                    "isVodOrCollection": true,
-                    "vodID": vodID
-                },
-                "extensions": {
-                    "persistedQuery": {
-                        "version": 1,
-                        "sha256Hash": "e1edae8122517d013405f237ffcc124515dc6ded82480a88daef69c83b53ac01"
-                    }
-                }
-            },
-            {
-                "operationName": "VodChannelLoginQuery",
-                "variables": {
-                    "videoID": vodID
-                },
-                "extensions": {
-                    "persistedQuery": {
-                        "version": 1,
-                        "sha256Hash": "0c5feea4dad2565508828f16e53fe62614edf015159df4b3bca33423496ce78e"
-                    }
-                }
-            }
-        ]),
+        body: JSON.stringify({
+            "query": "query { video(id: \"" + vodID + "\") { broadcastType, createdAt, seekPreviewsURL, owner { login } }}"
+        }),
         headers: {
             'Client-Id': 'kimne78kx3ncx6brgo4mv6wki5h1ko',
             'Accept': 'application/json',
@@ -64,6 +23,18 @@ function createServingID() {
     }
 
     return id;
+}
+
+async function isValidQuality(url) {
+    const response = await fetch(url);
+
+    if (response.ok) {
+        const data = await response.text();
+
+        return data.includes(".ts");
+    }
+
+    return false;
 }
 
 const oldFetch = self.fetch;
@@ -87,9 +58,8 @@ self.fetch = async function (url, opt) {
                 return new Response("Unable to fetch twitch data API", 403);
             }
 
-            const videoData = data[0].data.video;
-            const vodData = data[1].data.video;
-            const channelData = data[2].data.video.owner
+            const vodData = data.data.video;
+            const channelData = vodData.owner
 
             let resolutions = {
                 "160p30": {
@@ -106,6 +76,10 @@ self.fetch = async function (url, opt) {
                 },
                 "720p60": {
                     "res": "1280x720",
+                    "fps": 60
+                },
+                "1080p60": {
+                    "res": "1920x1080",
                     "fps": 60
                 },
                 "chunked": {
@@ -125,7 +99,7 @@ self.fetch = async function (url, opt) {
 
             resolutions = ordered_resolutions;
 
-            const currentURL = new URL(videoData.seekPreviewsURL);
+            const currentURL = new URL(vodData.seekPreviewsURL);
 
             const domain = currentURL.host;
             const paths = currentURL.pathname.split("/");
@@ -133,7 +107,6 @@ self.fetch = async function (url, opt) {
 
             let fakePlaylist = `#EXTM3U
 #EXT-X-TWITCH-INFO:ORIGIN="s3",B="false",REGION="EU",USER-IP="127.0.0.1",SERVING-ID="${createServingID()}",CLUSTER="cloudfront_vod",USER-COUNTRY="BE",MANIFEST-CLUSTER="cloudfront_vod"`;
-            let sources_ = [];
 
             const now = new Date("2023-02-10");
             const created = new Date(vodData.createdAt);
@@ -143,50 +116,38 @@ self.fetch = async function (url, opt) {
 
             const broadcastType = vodData.broadcastType.toLowerCase();
 
-            if (broadcastType === "highlight") {
-                for ([resKey, resValue] of Object.entries(resolutions)) {
-                    sources_.push({
-                        src: `https://${domain}/${vodSpecialID}/${resKey}/highlight-${vodId}.m3u8`,
-                        quality: resKey == "chunked" ? resValue.res.split("x")[1] + "p" : resKey,
-                        resolution: resValue.res,
-                        fps: Math.ceil(resValue.fps),
-                        enabled: resKey == "chunked" ? "YES" : "NO"
-                    });
-                };
-            } else if (broadcastType === "upload" && days_difference > 7) {
-                // Only old uploaded VOD works with this method now
-
-                for ([resKey, resValue] of Object.entries(resolutions)) {
-                    sources_.push({
-                        src: `https://${domain}/${channelData.login}/${vodId}/${vodSpecialID}/${resKey}/index-dvr.m3u8`,
-                        quality: resKey == "chunked" ? resValue.res.split("x")[1] + "p" : resKey,
-                        resolution: resValue.res,
-                        fps: Math.ceil(resValue.fps),
-                        enabled: resKey == "chunked" ? "YES" : "NO"
-                    });
-                };
-            } else {
-                for ([resKey, resValue] of Object.entries(resolutions)) {
-                    sources_.push({
-                        src: `https://${domain}/${vodSpecialID}/${resKey}/index-dvr.m3u8`,
-                        quality: resKey == "chunked" ? resValue.res.split("x")[1] + "p" : resKey,
-                        resolution: resValue.res,
-                        ps: Math.ceil(resValue.fps),
-                        enabled: resKey == "chunked" ? "YES" : "NO"
-                    });
-                }
-            }
-
             let startQuality = 8534030;
 
-            Object.entries(sources_).forEach(([_, value]) => {
-                fakePlaylist += `
-#EXT-X-MEDIA:TYPE=VIDEO,GROUP-ID="${value.quality}",NAME="${value.quality}",AUTOSELECT=${value.enabled},DEFAULT=${value.enabled}
-#EXT-X-STREAM-INF:BANDWIDTH=${startQuality},CODECS="avc1.64002A,mp4a.40.2",RESOLUTION=${value.resolution},VIDEO="${value.quality}",FRAME-RATE=${value.fps}
-${value.src}`;
+            for ([resKey, resValue] of Object.entries(resolutions)) {
+                var url = undefined;
 
-                startQuality -= 100;
-            });
+                if (broadcastType === "highlight") {
+                    url = `https://${domain}/${vodSpecialID}/${resKey}/highlight-${vodId}.m3u8`;
+                } else if (broadcastType === "upload" && days_difference > 7) {
+                    // Only old uploaded VOD works with this method now
+
+                    url = `https://${domain}/${channelData.login}/${vodId}/${vodSpecialID}/${resKey}/index-dvr.m3u8`;
+                } else {
+                    url = `https://${domain}/${vodSpecialID}/${resKey}/index-dvr.m3u8`;
+                }
+
+                if (url == undefined) {
+                    continue;
+                }
+
+                if (await isValidQuality(url)) {
+                    const quality = resKey == "chunked" ? resValue.res.split("x")[1] + "p" : resKey;
+                    const enabled = resKey == "chunked" ? "YES" : "NO";
+                    const fps = resValue.fps;
+
+                    fakePlaylist += `
+#EXT-X-MEDIA:TYPE=VIDEO,GROUP-ID="${quality}",NAME="${quality}",AUTOSELECT=${enabled},DEFAULT=${enabled}
+#EXT-X-STREAM-INF:BANDWIDTH=${startQuality},CODECS="avc1.64002A,mp4a.40.2",RESOLUTION=${resValue.res},VIDEO="${quality}",FRAME-RATE=${fps}
+${url}`;
+
+                    startQuality -= 100;
+                }
+            };
 
             const header = new Headers();
             header.append('Content-Type', 'application/vnd.apple.mpegurl');
