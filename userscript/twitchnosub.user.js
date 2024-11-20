4e3a2862b282113ccfee0b7bbf71251359c53676
besuper <16976439+besuper@users.noreply.github.com>
2024-11-20 21:27:36 +0100

    Fix player core variant in userscript

--- userscript/twitchnosub.user.js
+++ userscript/twitchnosub.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TwitchNoSub
 // @namespace    https://github.com/besuper/TwitchNoSub
-// @version      1.0.1
+// @version      1.0.2
 // @description  Watch sub only VODs on Twitch
 // @author       besuper
 // @match        *://*.twitch.tv/videos/*
@@ -17,11 +17,38 @@
 (function () {
     'use strict';
 
+    var isVariantA = false;
+    const originalAppendChild = document.head.appendChild;
+
+    document.head.appendChild = function (element) {
+        if (element.tagName === "SCRIPT") {
+            if (element.src.includes("player-core-variant-a")) {
+                isVariantA = true;
+            }
+        }
+
+        return originalAppendChild.call(this, element);
+    };
+
     const oldWorker = window.Worker;
 
     window.Worker = class Worker extends oldWorker {
         constructor() {
             super(URL.createObjectURL(new Blob(["importScripts('https://cdn.jsdelivr.net/gh/besuper/TwitchNoSub/src/patch_amazonworker.js', 'https://cdn.jsdelivr.net/npm/amazon-ivs-player/dist/assets/amazon-ivs-worker.min.js');"])));
+
+            if (!isVariantA) {
+                this.addEventListener("message", (event) => {
+                    const data = event.data;
+
+                    if (data.id == 1 && data.type == 1) {
+                        const newData = event.data;
+
+                        newData.arg = [data.arg];
+
+                        this.postMessage(newData);
+                    }
+                });
+            }
         }
     }
 })();
