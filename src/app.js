657a8bd7bba13b6f3dc1e8905e794e3a42ccd8fd
besuper <16976439+besuper@users.noreply.github.com>
2024-11-20 21:40:52 +0100

    Add player core variant support in main extension

---
+++ src/app.js
@@ -0,0 +1,34 @@
+var isVariantA = false;
+const originalAppendChild = document.head.appendChild;
+
+document.head.appendChild = function (element) {
+    if (element.tagName === "SCRIPT") {
+        if (element.src.includes("player-core-variant-a")) {
+            isVariantA = true;
+        }
+    }
+
+    return originalAppendChild.call(this, element);
+};
+
+const oldWorker = window.Worker;
+
+window.Worker = class Worker extends oldWorker {
+    constructor(twitchBlobUrl) {
+        super(twitchBlobUrl);
+
+        if (!isVariantA) {
+            this.addEventListener("message", (event) => {
+                const data = event.data;
+
+                if (data.id == 1 && data.type == 1) {
+                    const newData = event.data;
+
+                    newData.arg = [data.arg];
+
+                    this.postMessage(newData);
+                }
+            });
+        }
+    }
+}
---
+++ src/twitchnosub.js
@@ -0,0 +1,8 @@
+function injectScript(src) {
+    const s = document.createElement('script');
+    s.src = chrome.runtime.getURL(src);
+    s.onload = () => s.remove();
+    (document.head || document.documentElement).append(s);
+}
+
+injectScript("src/app.js");
