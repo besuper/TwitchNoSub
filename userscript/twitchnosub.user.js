b14cf41b2ae35ff5b1ddd71fbd149b423217fc36
trap <TrapG0d@proton.me>
2024-12-23 14:32:08 +0000

    Userscript Fix

--- userscript/twitchnosub.user.js
+++ userscript/twitchnosub.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TwitchNoSub
 // @namespace    https://github.com/besuper/TwitchNoSub
-// @version      1.0.2
+// @version      1.0.3
 // @description  Watch sub only VODs on Twitch
 // @author       besuper
 // @match        *://*.twitch.tv/videos/*
@@ -9,8 +9,6 @@
 // @updateURL    https://raw.githubusercontent.com/besuper/TwitchNoSub/master/userscript/twitchnosub.user.js
 // @downloadURL  https://raw.githubusercontent.com/besuper/TwitchNoSub/master/userscript/twitchnosub.user.js
 // @icon         https://raw.githubusercontent.com/besuper/TwitchNoSub/master/assets/icons/icon.png
-// @run-at       document-start
-// @inject-into  page
 // @grant        none
 // ==/UserScript==
 
@@ -18,11 +16,12 @@
     'use strict';
 
     var isVariantA = false;
+
     const originalAppendChild = document.head.appendChild;
 
     document.head.appendChild = function (element) {
         if (element.tagName === "SCRIPT") {
-            if (element.src.includes("player-core-variant-a")) {
+            if (element.src && element.src.includes("player-core-variant-a")) {
                 isVariantA = true;
             }
         }
@@ -34,15 +33,16 @@
 
     window.Worker = class Worker extends oldWorker {
         constructor() {
-            super(URL.createObjectURL(new Blob(["importScripts('https://cdn.jsdelivr.net/gh/besuper/TwitchNoSub/src/patch_amazonworker.js', 'https://cdn.jsdelivr.net/npm/amazon-ivs-player/dist/assets/amazon-ivs-worker.min.js');"])));
+            const blobUrl = URL.createObjectURL(new Blob([`importScripts('https://cdn.jsdelivr.net/gh/besuper/TwitchNoSub/src/patch_amazonworker.js', 'https://cdn.jsdelivr.net/npm/amazon-ivs-player/dist/assets/amazon-ivs-worker.min.js');`],));
+
+            super(blobUrl);
 
             if (!isVariantA) {
                 this.addEventListener("message", (event) => {
                     const data = event.data;
 
                     if (data.id == 1 && data.type == 1) {
-                        const newData = event.data;
-
+                        const newData = { ...data };
                         newData.arg = [data.arg];
 
                         this.postMessage(newData);
