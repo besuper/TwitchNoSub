5b5778b731a8049cd6b968fda5c4cd3e692abe9b
trap <TrapG0d@proton.me>
2025-01-03 16:04:18 +0000

    Bug Fixes

--- userscript/twitchnosub.user.js
+++ userscript/twitchnosub.user.js
@@ -1,17 +1,16 @@
 // ==UserScript==
 // @name         TwitchNoSub
 // @namespace    https://github.com/besuper/TwitchNoSub
-// @version      1.0.3
+// @version      1.0.4
 // @description  Watch sub only VODs on Twitch
 // @author       besuper
-// @match        *://*.twitch.tv/videos/*
-// @match        *://*.twitch.tv/*/video/*
 // @updateURL    https://raw.githubusercontent.com/besuper/TwitchNoSub/master/userscript/twitchnosub.user.js
 // @downloadURL  https://raw.githubusercontent.com/besuper/TwitchNoSub/master/userscript/twitchnosub.user.js
 // @icon         https://raw.githubusercontent.com/besuper/TwitchNoSub/master/assets/icons/icon.png
+// @match        *://*.twitch.tv/*
+// @inject-into  page
 // @grant        none
 // ==/UserScript==
-
 (function () {
     'use strict';
 
@@ -33,19 +32,19 @@
 
     window.Worker = class Worker extends oldWorker {
         constructor() {
-            const blobUrl = URL.createObjectURL(new Blob([`importScripts('https://cdn.jsdelivr.net/gh/besuper/TwitchNoSub/src/patch_amazonworker.js', 'https://cdn.jsdelivr.net/npm/amazon-ivs-player/dist/assets/amazon-ivs-worker.min.js');`],));
-
+            const blobUrl = URL.createObjectURL(new Blob([`
+                importScripts(
+                    'https://cdn.jsdelivr.net/gh/besuper/TwitchNoSub/src/patch_amazonworker.js',
+                    'https://cdn.jsdelivr.net/npm/amazon-ivs-player/dist/assets/amazon-ivs-worker.min.js'
+                );
+            `]));
             super(blobUrl);
 
             if (!isVariantA) {
                 this.addEventListener("message", (event) => {
-                    const data = event.data;
-
-                    if (data.id == 1 && data.type == 1) {
-                        const newData = { ...data };
-                        newData.arg = [data.arg];
-
-                        this.postMessage(newData);
+                    const { data } = event;
+                    if (data.id === 1 && data.type === 1) {
+                        this.postMessage({ ...data, arg: [data.arg] });
                     }
                 });
             }
