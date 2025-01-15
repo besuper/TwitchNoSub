6ee10113076c950aea581ffc16cab26bdcd67c75
trap <96237442+trapgod1@users.noreply.github.com>
2025-01-15 22:16:26 +0000

    Userscript Fix (#146)

--- userscript/twitchnosub.user.js
+++ userscript/twitchnosub.user.js
@@ -1,28 +1,28 @@
 // ==UserScript==
 // @name         TwitchNoSub
 // @namespace    https://github.com/besuper/TwitchNoSub
-// @version      1.0.2
+// @version      1.0.3
 // @description  Watch sub only VODs on Twitch
 // @author       besuper
-// @match        *://*.twitch.tv/videos/*
-// @match        *://*.twitch.tv/*/video/*
 // @updateURL    https://raw.githubusercontent.com/besuper/TwitchNoSub/master/userscript/twitchnosub.user.js
 // @downloadURL  https://raw.githubusercontent.com/besuper/TwitchNoSub/master/userscript/twitchnosub.user.js
 // @icon         https://raw.githubusercontent.com/besuper/TwitchNoSub/master/assets/icons/icon.png
-// @run-at       document-start
+// @match        *://*.twitch.tv/*
+// @run-at       document-end
 // @inject-into  page
 // @grant        none
-// ==/UserScript==
 
+// ==/UserScript==
 (function () {
     'use strict';
 
     var isVariantA = false;
+
     const originalAppendChild = document.head.appendChild;
 
     document.head.appendChild = function (element) {
-        if (element.tagName === "SCRIPT") {
-            if (element.src.includes("player-core-variant-a")) {
+        if (element?.tagName === "SCRIPT") {
+            if (element?.src?.includes("player-core-variant-a")) {
                 isVariantA = true;
             }
         }
@@ -34,18 +34,19 @@
 
     window.Worker = class Worker extends oldWorker {
         constructor() {
-            super(URL.createObjectURL(new Blob(["importScripts('https://cdn.jsdelivr.net/gh/besuper/TwitchNoSub/src/patch_amazonworker.js', 'https://cdn.jsdelivr.net/npm/amazon-ivs-player/dist/assets/amazon-ivs-worker.min.js');"])));
+            const blobUrl = URL.createObjectURL(new Blob([`
+                importScripts(
+                    'https://cdn.jsdelivr.net/gh/besuper/TwitchNoSub/src/patch_amazonworker.js',
+                    'https://cdn.jsdelivr.net/npm/amazon-ivs-player/dist/assets/amazon-ivs-worker.min.js'
+                );
+            `]));
+            super(blobUrl);
 
             if (!isVariantA) {
                 this.addEventListener("message", (event) => {
-                    const data = event.data;
-
-                    if (data.id == 1 && data.type == 1) {
-                        const newData = event.data;
-
-                        newData.arg = [data.arg];
-
-                        this.postMessage(newData);
+                    const { data } = event;
+                    if (data.id === 1 && data.type === 1) {
+                        this.postMessage({ ...data, arg: [data.arg] });
                     }
                 });
             }
