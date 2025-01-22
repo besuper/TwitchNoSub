351c3bd90ceb93f91b4ad0996a0999665c167485
susan kandel 1 <susankandel99@gmail.com>
2025-01-22 18:36:29 +0545

    Update app.js (Black screen fixed)  #164
    Showing black loading screen #164

--- src/app.js
+++ src/app.js
@@ -1,58 +1,32 @@
 var isVariantA = false;
-
 const originalAppendChild = document.head.appendChild;
 
 document.head.appendChild = function (element) {
-    if (element?.tagName === "SCRIPT") {
-        if (element?.src?.includes("player-core-variant-a")) {
-            isVariantA = true;
-        }
+  if (element.tagName === "SCRIPT") {
+    if (element.src.includes("player-core-variant-a")) {
+      isVariantA = true;
     }
+  }
 
-    return originalAppendChild.call(this, element);
+  return originalAppendChild.call(this, element);
 };
 
 const oldWorker = window.Worker;
 
 window.Worker = class Worker extends oldWorker {
-    constructor(twitchBlobUrl) {
-        super(twitchBlobUrl);
+  constructor(twitchBlobUrl) {
+    super(twitchBlobUrl);
 
-        console.log("[TNS] Patched worker with variant: " + (isVariantA ? "A" : "B"));
+    this.addEventListener("message", (event) => {
+      const data = event.data;
 
-        this.addEventListener("message", (event) => {
-            const { data } = event;
-            if ((data.id === 1 || isVariantA) && data.type === 1) {
-                // Sometimes there is a codec property undefined that cause Error 4000
-                // amazon-ivs-worker.min.js:2 Player stopping playback - error :2 (ErrorNotSupported code 4 - Failed to execute 'addSourceBuffer' on 'MediaSource': The type provided ('video/mp4;undefined') is unsupported.)
+      if ((data.id == 1 || isVariantA) && data.type == 1) {
+        const newData = event.data;
 
-                try {
-                    this.postMessage({ ...data, arg: [data.arg] });
-                } catch (e) {
-                    console.error("[TNS] Error when sending postMessage");
-                    console.error(e);
-                    console.error(data);
+        newData.arg = [data.arg];
 
-                    let isMediaSource = false;
-
-                    for (const element of data.arg) {
-                        if (typeof element === "object" && "srcObj" in element) {
-                            isMediaSource = true;
-                            break;
-                        }
-                    }
-
-                    if (isMediaSource) {
-                        // Sometimes data contains MediaSourceHandle that is non-cloneable
-                        // data.arg contains srcObj: MediaSourceHandle {}
-                        console.log("[TNS] MediaSourceHandle found, can't patch the message");
-                        return;
-                    }
-
-                    // In case of other errors, still try post message
-                    this.postMessage({ ...data, arg: [data.arg] });
-                }
-            }
-        });
-    }
-}
+        this.postMessage(newData);
+      }
+    });
+  }
+};
