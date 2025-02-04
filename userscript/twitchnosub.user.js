325a581325e8530c685ef3aa13f9f3546c81ecb5
besuper <16976439+besuper@users.noreply.github.com>
2025-02-04 19:13:23 +0100

    Update versions

--- userscript/twitchnosub.user.js
+++ userscript/twitchnosub.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TwitchNoSub
 // @namespace    https://github.com/besuper/TwitchNoSub
-// @version      1.1.0
+// @version      1.1.1
 // @description  Watch sub only VODs on Twitch
 // @author       besuper
 // @updateURL    https://raw.githubusercontent.com/besuper/TwitchNoSub/master/userscript/twitchnosub.user.js
